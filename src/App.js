45621d57ffb4d58dbd59e082b10925efece0ecaa
Samarth <samarthdave@users.noreply.github.com>
2018-04-03 18:40:27 -0500

    Added react router and configured to 3 sections

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Clock from './Clock';
 import './App.css';
 
 export default class App extends Component {
   render() {
-    const timer = {
-      textAlign: 'center'
-    };
     return (
-      <div className="pure-g">
-        <div className="pure-u-1-1">
-          <Clock />
-        </div>
-        <div className="pure-u-1-3">
-          <div className="btn l-box">
-            <h3>Countup Timer</h3>
+      <div>
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <Clock />
           </div>
         </div>
-        <div className="pure-u-1-3">
-          <div className="btn l-box">
-            <h3>Countdown Timer</h3>
+        <BrowserRouter>
+          <div className="pure-g">
+            <div className="pure-u-1-3">
+              <div className="btn l-box">
+                <h3>Countup Timer</h3>
+              </div>
+            </div>
+            <div className="pure-u-1-3">
+              <div className="btn l-box">
+                <h3>Countdown Timer</h3>
+              </div>
+            </div>
+            <div className="pure-u-1-3">
+              <div className="btn l-box">
+                <h3>Pomodoro Clock</h3>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="pure-u-1-3">
-          <div className="btn l-box">
-            <h3>Pomodoro Clock</h3>
-          </div>
-        </div>
+        </BrowserRouter>
       </div>
     );
   }
